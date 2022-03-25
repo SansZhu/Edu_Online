7ccd52ba23729b31e34fdaafe644f3ba910f91c4
SansZhu <707643309@qq.com>
2022-03-25 21:14:30 +0800

    Initial commit

---
+++ src/main/java/top/zshan/eduonline/controller/MainController.java
@@ -0,0 +1,22 @@
+package top.zshan.eduonline.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @author SansZhu
+ * @create 2022/3/24 17:20
+ */
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String indexPage(){
+        return "redirect:/index.html";
+    }
+
+    @GetMapping("/index.html")
+    public String index(){
+        return "index";
+    }
+}
